f1034b00a4d0af0d3b2b170e6348d6088f58a968
Donald-Edinam <kobbyedinamdonald@gmail.com>
2023-03-15 09:28:33 +0000

    Main.js updated

--- src/main.js
+++ src/main.js
@@ -22,7 +22,27 @@
    // Day 21
    "But those who hope in the Lord will renew their strength. They will soar on wings like eagles; they will run and not grow weary, they will walk and not be faint.",
    "We wait in hope for the Lord; he is our help and our shield. In him our hearts rejoice, for we trust in his holy name. May your unfailing love be with us, Lord, even as we put our hope in you.",
-   ""
+   "I wait for the Lord, my whole being waits, and in his word I put my hope. I wait for the Lord more than watchmen wait for the morning.",
+   "But as for me, I watch in hope for the Lord, I wait for God my Saviour ; my God will hear me.",
+   "Humble yourselves, therefore, under God’s mighty hand, that he may lift you up in due time. Cast all your anxiety on him because he cares for you.",
+   "For through the Spirit we eagerly await by faith the righteousness for which we hope.",
+   "Then you will call on me and come and pray to me, and I will listen to you.",
+   // Day 28 - 35
+   "We wait in hope for the Lord; he is our help and our shield. In him our hearts rejoice, for we trust in his holy name. May your unfailing love be with us, Lord, even as we put our hope in you.",
+   "For God alone, O my soul, wait in silence, for my hope is from him.",
+   "Show me your ways, Lord, teach me your paths. Guide me in your truth and teach me, for you are God my Saviour , and my hope is in you all day long.",
+   "At once the Spirit sent him out into the wilderness, and he was in the wilderness forty days, being tempted[a] by Satan. He was with the wild animals, and angels attended him. After John was put in prison, Jesus went into Galilee, proclaiming the good news of God. “The time has come,” he said. “The kingdom of God has come near. Repent and believe the good news!”",
+   "Then Jesus was led by the Spirit into the wilderness to be tempted by the devil. After fasting forty days and forty nights, he was hungry. The tempter came to him and said, “If you are the Son of God, tell these stones to become bread.” Jesus answered, “It is written: ‘Man shall not live on bread alone, but on every word that comes from the mouth of God .’” Then the devil took him to the holy city and had him stand on the highest point of the temple. “If you are the Son of God,” he said, “throw yourself down. For it is written: “‘He will command his angels concerning you, and they will lift you up in their hands, so that you will not strike your foot against a stone.’” Jesus answered him, “It is also written: ‘Do not put the Lord your God to the test.” Again, the devil took him to a very high mountain and showed him all the kingdoms of the world and their splendor. “All this I will give you,” he said, “if you will bow down and worship me.” Jesus said to him, “Away from me, Satan! For it is written: ‘Worship the Lord your God, and serve him only.’” Then the devil left him, and angels came and attended him.",
+   "I want to know Christ—yes, to know the power of his resurrection and participation in his sufferings, becoming like him in his death, and so, somehow, attaining to the resurrection from the dead.",
+   " The thief comes only to steal and kill and destroy; I have come that they may have life, and have it to the full.",
+   "I have told you these things, so that in me you may have peace. In this world you will have trouble. But take heart! I have overcome the world.",
+   
+   // Day 36 - 40
+   "The Lord your God is with you, the Mighty Warrior who saves. He will take great delight in you; in his love he will no longer rebuke you, but will rejoice over you with singing",
+   "Just as Moses lifted up the snake in the wilderness, so the Son of Man must be lifted up, that everyone who believes may have eternal life in him .” For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life. For God did not send his Son into the world to condemn the world, but to save the world through him . Whoever believes in him is not condemned, but whoever does not believe stands condemned already because they have not believed in the name of God’s one and only Son.",
+   "He will stand and shepherd his flock in the strength of the Lord, in the majesty of the name of the Lord his God. And they will live securely, for then his greatness will reach to the ends of the earth.",
+   "Blow the trumpet in Zion; sound the alarm on my holy hill. Let all who live in the land tremble, for the day of the Lord is coming. It is close at hand— a day of darkness and gloom, a day of clouds and blackness. Like dawn spreading across the mountains a large and mighty army comes, such as never was in ancient times nor ever will be in ages to come.",
+   "Rejoice greatly, Daughter Zion! Shout, Daughter Jerusalem! See, your king comes to you, righteous and victorious, lowly and riding on a donkey, on a colt, the foal of a donkey."
    ]
  
  
@@ -183,131 +203,148 @@
       
       // Day 22 - 28
      {
-        book: 'Philippians ',
-        chapter: 4,
-        verse: ' 1 - 5',
+        book: 'Psalms ',
+        chapter: 33,
+        verse: ' 20 - 22',
         reading: reading[21],
         day: 22
       },
       {
-        book: 'Philippians',
-        chapter: 4,
-        verse: ' 1 - 5',
+        book: 'Psalms ',
+        chapter: 130,
+        verse: ' 5 - 6',
         reading: reading[22],
         day: 23
       },
       {
-        book: 'Philippians',
-        chapter: 4,
-        verse: ' 1 - 5',
+        book: 'Micah  ',
+        chapter: 7,
+        verse: ' 7',
         reading: reading[23],
         day: 24
       },
       {
-        book: 'Philippians',
-        chapter: 4,
-        verse: ' 1 - 5',
+        book: '1 Peter  ',
+        chapter: 5,
+        verse: ' 6 - 7',
         reading: reading[24],
         day: 25
       },
       {
-        book: 'Philippians',
-        chapter: 4,
-        verse: ' 1 - 5',
+        book: 'Galatians  ',
+        chapter: 5,
+        verse: ' 5',
         reading: reading[25],
         day: 26
       },
       {
-        book: 'Philippians',
-        chapter: 4,
-        verse: ' 1 - 5',
+        book: 'Jeremiah ',
+        chapter: 29,
+        verse: ' 5',
         reading: reading[26],
         day: 27
       },
       {
-        book: 'Philippians',
-        chapter: 4,
-        verse: ' 1 - 5',
+        book: 'Psalms ',
+        chapter: 33,
+        verse: ' 20 - 22',
         reading: reading[27],
         day: 28
       },
       
       //Day 29  - 35
      {
-        book: 'Philippians',
-        chapter: 4,
-        verse: ' 1 - 5',
-        reading: reading
+        book: 'Psalms ',
+        chapter: 62,
+        verse: ' 5',
+        reading: reading[28],
+        day: 29
       },
       {
-        book: 'Philippians',
-        chapter: 4,
-        verse: ' 1 - 5',
-        reading: reading
+        book: 'Psalms ',
+        chapter: 25,
+        verse: ' 4 - 5',
+        reading: reading[29],
+        day: 30
       },
       {
-        book: 'Philippians',
-        chapter: 4,
-        verse: ' 1 - 5',
-        reading: reading
+        book: 'Mark ',
+        chapter: 1,
+        verse: ' 12 - 15',
+        reading: reading[30],
+        day: 31
       },
       {
-        book: 'Philippians',
+        book: 'Matthew  ',
         chapter: 4,
-        verse: ' 1 - 5',
-        reading: reading
+        verse: ' 1 - 11',
+        reading: reading[31],
+        day: 32
       },
       {
-        book: 'Philippians',
-        chapter: 4,
-        verse: ' 1 - 5',
-        reading: reading
+        book: 'Philippians  ',
+        chapter: 3,
+        verse: ' 10 - 11',
+        reading: reading[32],
+        day: 33
       },
       {
-        book: 'Philippians',
-        chapter: 4,
-        verse: ' 1 - 5',
-        reading: reading
+        book: 'John ',
+        chapter: 10,
+        verse: ' 10',
+        reading: reading[33],
+        day: 34
       },
       {
-        book: 'Philippians',
-        chapter: 4,
-        verse: ' 1 - 5',
-        reading: reading
+        book: 'John ',
+        chapter: 16,
+        verse: ' 33',
+        reading: reading[34],
+        day: 35
       },
       
       // Day 36 - 40
      {
-        book: 'Philippians',
-        chapter: 4,
-        verse: ' 1 - 5',
-        reading: reading
+        book: 'Zephaniah  ',
+        chapter: 3,
+        verse: ' 17',
+        reading: reading[35],
+        day: 36
       },
       {
-        book: 'Philippians',
-        chapter: 4,
-        verse: ' 1 - 5',
-        reading: reading
+        book: 'John ',
+        chapter: 3,
+        verse: ' 14 - 18',
+        reading: reading[36],
+        day: 37
       },
       {
-        book: 'Philippians',
-        chapter: 4,
-        verse: '1 - 5',
-        reading: reading
+        book: 'Micah  ',
+        chapter: 5,
+        verse: ' 4',
+        reading: reading[37],
+        day: 38
       },
       {
-        book: 'Philippians',
-        chapter: 4,
-        verse: ' 1 - 5',
-        reading: reading
+        book: 'Joel ',
+        chapter: 2,
+        verse: ' 1 - 2',
+        reading: reading[38],
+        day: 39
       },
-
+      {
+        book: 'Zechariah  ',
+        chapter: 9,
+        verse: ' 9',
+        reading: reading[39],
+        day: 40
+      }
     ];
 
     function getDailyVerse() {
       const today = new Date();
       const dayOfYear = Math.floor((today - new Date(today.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
-      const verseIndex = dayOfYear % verses.length - 14;
+      const verseIndex = dayOfYear % verses.length - 13;
       const verse = verses[verseIndex];
 
       // Display the daily verse on the website
